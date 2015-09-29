41fb03f65709e89c7107aea937e1897a04adb422
Liam Oehlman <liam.oehlman@gmail.com>
2015-09-29 13:33:35 +1000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mr-potato-head
 Get an arbitrary, common mysql lock
+
+    var mrPotatoHead = require('mr-potato-head');
+
+    mrPotatoHead({
+        host: 'localhost',
+        username: 'database',
+        password: 'databasepassword',
+        database: 'databasename'
+    }).lock(function(error) {
+        if (error) {
+            // no lock for you!
+        }
+    });
---
+++ index.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql'),
+    lockQuery = 'SELECT GET_LOCK(?, ?)';
+
+function mrPotatoHead(config) {
+    var lockWaitTimeout = config.lockWaitTimeout || 5,
+        lockName = config.lockName || 'mr-potato-head',
+        connection = mysql.createConnection({
+            host: config.host,
+            user: config.user || config.username,
+            password: config.password,
+            database: config.database
+        });
+
+    connection.connect();
+
+    return {
+        lock: function(callback) {
+            connection.query(lockQuery, [lockName, lockWaitTimeout], function(error, rows) {
+                if (error) {
+                    return callback(error);
+                }
+
+                callback(!rows[0][Object.keys(rows[0])]);
+            });
+        },
+        close: function(callback) {
+            connection.end(callback);
+        }
+    };
+}
+
+module.exports = mrPotatoHead;
